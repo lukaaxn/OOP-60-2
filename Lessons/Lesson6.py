0852b501e3147ca65ef4fa2918b2c1f6e745a788
lukaaxn <ayelitaaxn@gmail.com>
2025-11-18 13:32:49 +0600

    feat: added Lesson6.py

---
+++ Lessons/Lesson6.py
@@ -0,0 +1,14 @@
+def get_element_by_index(array, target):
+    # for i in array:
+    #     if i == target:
+    #         return print('We found it!')
+    #     else:
+    #         return print('We cant found it')
+
+    # if target in array:
+    #     return print('We found it!')
+    # return print('We cant found it')
+
+    return print(array[target])
+
+get_element_by_index([1,2,3,4,5,6,7],5)
